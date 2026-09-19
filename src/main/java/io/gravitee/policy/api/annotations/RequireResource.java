f2d20abb6eb109ad458795db88fc6cdb783f979f
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-09-19 20:39:55 +0200

    build: drop what the parent already provides
    Plugin versions and the properties that fed them: the parent is the single
    place where they are decided, downgrades included.
    
    https://gravitee.atlassian.net/browse/BX-403

--- src/main/java/io/gravitee/policy/api/annotations/RequireResource.java
+++ src/main/java/io/gravitee/policy/api/annotations/RequireResource.java
@@ -25,5 +25,4 @@ import java.lang.annotation.Target;
  */
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RequireResource {
-}
+public @interface RequireResource {}
